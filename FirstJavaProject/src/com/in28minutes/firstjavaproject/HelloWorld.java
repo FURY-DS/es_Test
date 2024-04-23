ffe80695ee5ed1fc966d1aa8b3225f1bb9eb2c44
FURY-DS <57359649+FURY-DS@users.noreply.github.com>
2024-04-23 14:41:11 +0800

    20240423

--- FirstJavaProject/src/com/in28minutes/firstjavaproject/HelloWorld.java
+++ FirstJavaProject/src/com/in28minutes/firstjavaproject/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelldfdfoWordfdfdddld Fuddddck tdddddfdffdfhejhjhuddfdfdffdfj WORLD!!!!!!!");
+		System.out.println("Hello WORLD!!!!!!!");
 	}
 
 }
