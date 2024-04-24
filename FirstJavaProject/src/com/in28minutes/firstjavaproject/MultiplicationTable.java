f85aa097cbc017793b42410be908b51b1567cf4c
FURY-DS <57359649+FURY-DS@users.noreply.github.com>
2024-04-24 14:58:56 +0800

    dddd

--- FirstJavaProject/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
+++ FirstJavaProject/src/com/in28minutes/firstjavaproject/MultiplicationTable.java
@@ -4,15 +4,12 @@ public class MultiplicationTable {
 
 	
 	void print() {
-		for(int i = 1; i<=10; i++) {
-			System.out.printf("%d * %d = %d",5 ,i , 5 * i).println();
-		}
+		
+		print(5);
 	}
 	
 	void print(int table) {
-		for(int i = 1; i<=10; i++) {
-			System.out.printf("%d * %d = %d",table11 ,i , table * i).println();
-		}
+		print(table, 1, 10);
 	}
 	
 	void print(int table, int from, int to) {
--- FirstJavaProject/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
+++ FirstJavaProject/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -4,9 +4,9 @@ public class MultiplicationTableRunner {
 
 	public static void main(String[] args) {
 		MultiplicationTable table = new MultiplicationTable();
-		//table.print();
-		//table.print(6);
-		table.print(6, 11,20 );
+		table.print();
+		table.print(6);
+		table.print(6, 11, 20);
 	}
 
 }
